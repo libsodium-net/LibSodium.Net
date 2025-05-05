37fa4442bf1b8bc4a9cd0d27ea846e7698d5cffe
Jesus López <jesus.lopez@inercya.com>
2025-05-05 09:15:43 +0100

    build and test mac

--- src/build-test-aot-win.cmd
+++ src/build-test-aot-win.cmd
@@ -1,2 +1,2 @@
 @REM dotnet restore ./LibSodium.Net.Tests/LibSodium.Net.Tests.csproj -r win-x64
-dotnet publish ./LibSodium.Net.Tests/LibSodium.Net.Tests.csproj -c Release -f net8.0 -r win-x64 /p:PublishAot=true /p:SelfContained=true /p:PublishTrimmed=true /p:DefineConstants=AOT
+dotnet publish ./LibSodium.Net.Tests/LibSodium.Net.Tests.Win.csproj -c Release
